103c1894a129b4794ce6f7fb1336c30b00a38b9f
Daniel Kim <danieljoonkimm@gmail.com>
2019-01-15 01:51:28 -0800

    [edit] fixing landing page

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -9,12 +9,20 @@ class Home extends Component {
   };
 
   render() {
+    //i will need the nav bar
+    //need categories i.e most popular sports, featured esports game, etc.
+    //need login / join
+    //
     return(
       <div>
         Welcome to the landing page
+        <div>
         <ul>
           <li><Link to='./login'>Login</Link></li>
         </ul>
+        </div>
+        
+        <div></div>
       </div>
     )
   }
